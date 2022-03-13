bfd4f0615cf9bd47c0e68bc6dbce5d0daa6e2c9d
EddyBOUGHIOUL <eebo@globazch>
2022-03-13 16:49:16 +0100

    Ajoutez des fichiers projet.

---
+++ HydraWallet/HydraWallet/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using HydraWallet.ViewModels;
+using HydraWallet.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HydraWallet
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ HydraWallet/HydraWallet/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HydraWallet.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HydraWallet.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
